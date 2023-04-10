d294c1b617f90c32c020c0bd24dae7addad20c38
Isaac Poplin <caasinilpop@hotmail.com>
2023-04-10 16:44:08 -0400

    Bindings for request parsers

--- Protocol/python/pyspam.cpp
+++ Protocol/python/pyspam.cpp
@@ -1,17 +1,18 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "spam_api.hpp"
 
 
 PYBIND11_MODULE(pyspam, m) {
     auto m_gen = m.def_submodule("gen");
 
-    auto m_request = m_gen.def_submodule("request");
-    m_request.def("connect", &spam_api::gen::request::connect);
-    m_request.def("join", &spam_api::gen::request::join);
-    m_request.def("post", &spam_api::gen::request::post);
-    m_request.def("message", &spam_api::gen::request::message);
-    m_request.def("leave", &spam_api::gen::request::leave);
-    m_request.def("getusers", &spam_api::gen::request::getusers);
+    auto m_g_request = m_gen.def_submodule("request");
+    m_g_request.def("connect", &spam_api::gen::request::connect);
+    m_g_request.def("join", &spam_api::gen::request::join);
+    m_g_request.def("post", &spam_api::gen::request::post);
+    m_g_request.def("message", &spam_api::gen::request::message);
+    m_g_request.def("leave", &spam_api::gen::request::leave);
+    m_g_request.def("getusers", &spam_api::gen::request::getusers);
 
     auto m_respond = m_gen.def_submodule("respond");
     m_respond.def("connect", &spam_api::gen::respond::connect);
@@ -20,4 +21,14 @@ PYBIND11_MODULE(pyspam, m) {
     m_respond.def("message", &spam_api::gen::respond::message);
     m_respond.def("leave", &spam_api::gen::respond::leave);
     m_respond.def("getusers", &spam_api::gen::respond::getusers);
+
+    auto m_parse = m.def_submodule("parse");
+
+    auto m_p_request = m_parse.def_submodule("request");
+    m_p_request.def("connect", &spam_api::parse::request::connect);
+    m_p_request.def("join", &spam_api::parse::request::join);
+    m_p_request.def("post", &spam_api::parse::request::post);
+    m_p_request.def("message", &spam_api::parse::request::message);
+    m_p_request.def("leave", &spam_api::parse::request::leave);
+    m_p_request.def("getusers", &spam_api::parse::request::getusers);
 }
