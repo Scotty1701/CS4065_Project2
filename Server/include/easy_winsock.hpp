e3e5d3901fecbd7f4a2d7f216df782fbe0555583
Isaac Poplin <caasinilpop@hotmail.com>
2023-04-12 17:21:53 -0400

    First pass functions for easier socket interaction

---
+++ Server/include/easy_winsock.hpp
@@ -0,0 +1,43 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+
+class WinsockManager {
+    private:
+        SOCKET listenSocket;
+        std::vector<SOCKET> clientSockets;
+        WSADATA wsaData;
+        struct addrinfo* result;
+        struct addrinfo hints;
+
+    public:
+        WinsockManager();
+
+        SOCKET get_client_socket(int index);
+
+        int init_winsock(std::string port);
+
+        int create_listener();
+
+        int accept_client();
+
+        int receive_from_client(SOCKET clientSocket, char buffer[], int bufferLen);
+
+        void send_to_client(SOCKET clientSocket, char buffer[], int bufferLen);
+
+        int close_connection(SOCKET clientSocket);
+
+        void close_all_connections();
+
+        void shutdown_winsock();
+};
