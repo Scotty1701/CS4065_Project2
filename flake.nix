11700d4ed3752f2e0ec87e456803e73f8d7b689a
David Lewis <davidalewis00@gmail.com>
2023-04-12 08:56:16 -0400

    nix build for python bindings

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "";
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = { flake-utils.url = "github:numtide/flake-utils";
+             json-cpp = {
+               url = "github:Scotty1701/jsoncpp";
+               flake = false;
+             };
+             pybind11 = {
+               url = "github:pybind/pybind11";
+               flake = false;
+             };
+           };
+  outputs = { self, nixpkgs, flake-utils, json-cpp, pybind11 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -14,6 +23,37 @@
         ]);
       in
       rec {
+
+        packages = flake-utils.lib.flattenTree rec {
+          default = with pkgs; stdenv.mkDerivation rec{
+            name = "pyspam";
+            src = ./Protocol;
+            nativeBuildInputs = [cmake jsoncpp python3];
+            cmakeFlags = [
+              "-DFETCHCONTENT_SOURCE_DIR_JSON-CPP=${json-cpp}"
+              " -DFETCHCONTENT_SOURCE_DIR_PYBIND11=${pybind11}"
+            ];
+            configurePhase = ''
+              mkdir build && cd build
+              cmake ${(pkgs.lib.concatStrings cmakeFlags)} ..
+              cmake --build . --config Release
+              cmake --install . --prefix ../install
+              cd ../python
+              mkdir build && cd build
+              cmake ${(pkgs.lib.concatStrings cmakeFlags)} ..
+              cmake --build . --config Release
+              cmake --install . --prefix ../install
+            '';
+            buildPhase = ''
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r ../../install $out/
+              mkdir $out/python
+              cp -r ../install $out/python/
+            '';
+          };
+        };
         devShells = flake-utils.lib.flattenTree {
           default = with pkgs; mkShell {
             buildInputs = [ python-with-my-packages ];
